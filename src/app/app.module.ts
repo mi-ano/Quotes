c5cd0398edebc279c100b7bd12c000d221d13f95
mi-ano <ryanmiano68@gmail.com>
2019-07-07 23:10:42 +0300

    created component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteDescriptionComponent } from './quote-description/quote-description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteDescriptionComponent
   ],
   imports: [
     BrowserModule,
